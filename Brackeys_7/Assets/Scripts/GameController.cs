f328341ae49f893bf46c3487540e3b02f8f2e460
Felipe Inoue <40900330+fiwon123@users.noreply.github.com>
2022-02-22 23:52:00 -0300

    test

--- Brackeys_7/Assets/Scripts/GameController.cs
+++ Brackeys_7/Assets/Scripts/GameController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log("Teste");
+        }
     }
 
     // Update is called once per frame
